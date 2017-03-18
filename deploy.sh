1d6a84fc15ad43889243ef0628713f8abe92f6d3
danlou <daniel.b.loureiro+github@gmail.com>
2017-03-18 19:07:28 +0000

    Pelican output

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+PELICAN_DIR="~/projects/pelican-blog"
+GHPAGES_DIR="~/projects/danlou.github.io"
+
+cd $PELICAN_DIR
+pelican content -s publishconf.py
+mv output/* $GHPAGES_DIR
+cd $GHPAGES_DIR
+git add .
+git commit -m 'Pelican output'
+git push origin master
