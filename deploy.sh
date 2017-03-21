5c5a1d8132df60d256991b72c6a77a9bf491b097
danlou <daniel.b.loureiro+github@gmail.com>
2017-03-21 17:24:36 +0000

    Pelican output

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-PELICAN_DIR="~/projects/pelican-blog/"
-GHPAGES_DIR="~/projects/danlou.github.io/"
+PELICAN_DIR="/Users/danlou/projects/pelican-blog/"
+GHPAGES_DIR="/Users/danlou/projects/danlou.github.io/"
 
 cd $PELICAN_DIR
 pelican content -s publishconf.py
-mv output/* $GHPAGES_DIR
+rsync -a --remove-source-files output/* $GHPAGES_DIR
 cd $GHPAGES_DIR
 git add .
 git commit -m 'Pelican output'
